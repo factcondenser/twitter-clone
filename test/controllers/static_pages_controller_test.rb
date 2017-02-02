d74a08663060825ebdb7d360d2466a2304757d78
factcondenser <markpan@live.com>
2017-02-02 10:35:11 +0800

    Write test to make sure root route is set to home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 		@base_title = "Ruby on Rails Tutorial Twitter Clone"
   end
 
+  test "shoud get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
