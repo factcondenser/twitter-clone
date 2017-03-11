1d6d4b63b2d67c8ad385d3cc29f2171307ad90f6
factcondenser <markpan@live.com>
2017-03-11 23:54:27 +0800

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,27 +5,52 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
-	# Provides access to current user.
-	def current_user
-		# Syntax 1
-		#if @current_user.nil?
-		#	@current_user = User.find_by(id: session[:user_id])
-		#else
-		#	@current_user
-
-		# Syntax 2
-		#@current_user = @current_user || User.find_by(id: session[:user_id])
-
-		# Correct Syntax
-		@current_user ||= User.find_by(id: session[:user_id])
+	# Remembers a user in a persistent session.
+	def remember(user)
+		user.remember
+		cookies.permanent.signed[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
 	end
 
+	# Returns the curent logged-in user (if any).
+	def current_user
+		if (user_id = session[:user_id])
+			# Syntax 1
+			#if @current_user.nil?
+			#	@current_user = User.find_by(id: session[:user_id])
+			#else
+			#	@current_user
+
+			# Syntax 2
+			#@current_user = @current_user || User.find_by(id: session[:user_id])
+
+			# Correct Syntax
+			@current_user ||= User.find_by(id: user_id)
+		elsif (user_id = cookies.signed[:user_id])
+			# raise # The tests still pass, so this branch is currently untested.
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
+	end
+
+	# Returns true if the user is logged in, false otherwise.
 	def logged_in?
 		!current_user.nil?
 	end
 
+	# Forgets a persistent sessions.
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
 	# Logs out the current user.
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		# Technically unnecessary since @current_user created after destroy action and followed immediately by redirect.
 		@current_user = nil
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-	before_save { email.downcase! }
+	# Attributes
+	attr_accessor :remember_token
+
+	before_save { email.downcase! } # Also can be written as self.email = email.downcase
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i 
 	validates :email, presence: true, length: { maximum: 255 },
@@ -8,9 +11,34 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 
-	# Returns the hash digest of the given string.
-	def User.digest(string)
-		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-		BCrypt::Password.create(string, cost: cost)
+	# Class methods (idiomatically correct, if confusing)
+	class << self
+		# Returns the hash digest of the given string.
+		def digest(string)
+			cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+			BCrypt::Password.create(string, cost: cost)
+		end
+
+		# Returns a random token.
+		def new_token
+			SecureRandom.urlsafe_base64
+		end
+	end
+
+	# Instance methods
+	# Remembers a user in the database for use in persistent sessions.
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	# Returns true if the given token matches the digest.
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
 	end
 end
