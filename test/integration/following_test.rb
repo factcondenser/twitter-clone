0a9a9215bde1a13672fa8595fa28957a62c5e8fe
factcondenser <markpan@live.com>
2017-05-10 17:36:50 +0800

    Update integration tests and  microposts fixtures

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -49,11 +49,18 @@ class FollowingTest < ActionDispatch::IntegrationTest
     end
   end
 
-   test "should unfollow a user with Ajax" do
+  test "should unfollow a user with Ajax" do
     @user.follow(@other)
     relationship = @user.active_relationships.find_by(followed_id: @other.id)
     assert_difference '@user.following.count', -1 do
       delete relationship_path(relationship), xhr: true
+    end
+  end
+
+  test "feed on Home page" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
     end
   end
 end
